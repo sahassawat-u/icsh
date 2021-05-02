749593c1f9f3bf4caa949832dc14ef18cad18f81
Boss Udomsuktham <bossiccs@Bosss-MacBook-Pro.local>
2021-05-02 22:47:52 +0700

    0.2.0

--- icsh.c
+++ icsh.c
@@ -1,21 +1,10 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
-#define ARG_SIZE 1
-// enum instruction{ echo, !! };
-
-// enum instruction get_enum_value(char * val) {
-//     static char const * str[] = { "echo"};
-//     for (int i = 0; i < echo; ++i)
-//         if (!strcmp(str[i], val))
-//             return i;
-//     return END;
-//  }
-void read_line() {
-    char command_line[64];
-    // scanf("%[^\n]",command_line);
-    fgets(command_line,25,stdin);
-    char *token = strtok(command_line, " ");
+#define MAX_LEN 256
+char last_command[64];
+void read_line(char *line) {
+    char *token = strtok(line, " ");
     char **arr = NULL;
     int i = 0, len_count = 0;
     while (token != NULL)
@@ -51,7 +40,17 @@ void read_line() {
     }
     else if (strcmp(arr[0],"exit")==0) {
         exit(atoi(arr[1]));
-    } else {
+    } 
+    else if (strcmp(arr[0],"!!\n")==0) {
+        size_t n_last_command = strlen(last_command);
+        last_command[--n_last_command] = '\0';
+        printf("%s",last_command);
+        char input[64];
+        fgets(input,25,stdin);
+        read_line(last_command);
+        printf("\n");
+    }
+    else {
         printf("bad command\n");
     }
     // printf("%zu\n", n);
@@ -73,12 +72,22 @@ void start_shell(){
     // char x[64];
     while(1){
         printf("icsh $ ");
+        char command_line[64];
+    // scanf("%[^\n]",command_line);
+        fgets(command_line,100,stdin);
         // scanf("%s",x);
         // char command_line[64];
         // fgets(x,25,stdin);
+        // char *a = strrchr(x, '.');
         // scanf("%[^\n]",x);
-        // printf("%s",x);
-        read_line();
+        // printf("%s",a);
+        // printf("%s",last_command);
+        char tmp_command[64];
+        strcpy(tmp_command, command_line);
+        read_line(command_line);
+        // printf("AFTER\n");
+        strcpy(last_command, tmp_command);
+        // printf("%s",last_command);
         // fgets(command, sizeof command, stdin);
         // switch (echo) {
         // if (strcmp(command,"echo")) {
@@ -91,8 +100,29 @@ void start_shell(){
         // scanf("");
     };
 }
+void read_file(FILE *file){
+    char buffer[MAX_LEN];
+    while (fgets(buffer, MAX_LEN - 1, file) != NULL)  {
+        read_line(buffer);
+    }
+}
 
-int main(){
-    start_shell();
+int main(int argc, char **argv){
+    // Have argument(s)
+    if (argc > 1) {
+        for (int i = 1; i < argc; ++i) {
+        // printf("argv[%d]: %s\n", i, argv[i]);
+        FILE* fp;
+        fp = fopen(argv[i], "r");
+        if (fp == NULL) {
+            perror("Failed: ");
+            return 1;
+        }
+        read_file(fp);
+        }
+    }
+    else {
+        start_shell();
+    }
     return 0;
 }
